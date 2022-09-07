54e26d528fdbba12fbd66685a4f960fc7f99a58c
dingsaier <dse250073@gmail.com>
2022-09-07 22:51:35 +0800

    Feature[Init]: init uni-app front end

---
+++ QuickStart/pages/index/index.vue
@@ -0,0 +1,33 @@
+<template>
+    <view class="container">
+        HelloWorld, ChongBao
+    </view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				mark: 0
+			}
+		},
+		methods: {
+			addMark () {
+				//通过this.调用data对象中定义的变量
+				console.log("mark变量为：", this.mark)
+			}
+		},
+		mounted() {
+			this.addMark()
+		}
+	}
+</script>
+
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
